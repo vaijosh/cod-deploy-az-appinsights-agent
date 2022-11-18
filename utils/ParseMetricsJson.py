569099cd2c480d0f3586fac25026c2cd1109bf8f
vaijosh <vaijosh@hotmail.com>
2022-11-18 11:41:41 +0530

    ODX-2365:Explore integration with cloud - Added small utility to parse Hbase metrics and list for jmxMetrics box. Also modified applicationinsights.json and included all Hbase metrics by default

---
+++ utils/ParseMetricsJson.py
@@ -0,0 +1,21 @@
+import json
+
+f = open("HbaseMetrics.json")
+
+inputJson = json.load(f)
+
+keysToSkip = {'description', 'modelerType', 'tag.Context', 'tag.Hostname', 'name'}
+
+outputJson = []
+for i in inputJson:
+  objectName = i['name']
+  for key in i:
+    if key not in keysToSkip:
+      attribute = key
+      data = {}
+      data['objectName'] = objectName
+      data['attribute'] = attribute
+      if isinstance(i[key], dict):
+        data['name'] = i[key]['description']
+        outputJson.append(data)
+print(outputJson)
